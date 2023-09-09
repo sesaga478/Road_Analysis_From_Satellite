ce3cd6b32ad611a281f7ce87d740a20fd4167c2a
Sebastián Salazar <54647017+sesaga478@users.noreply.github.com>
2023-09-09 17:01:25 -0500

    Update city_from_coord.py

--- scripts/city_from_coord.py
+++ scripts/city_from_coord.py
@@ -5,6 +5,7 @@ def city_from_coord(latitude, longitude):
     geolocator = Nominatim(user_agent="reverse_geocoding_example")
     location = geolocator.reverse((latitude, longitude), exactly_one=True)
     if location:
+        address = location.raw.get('address', {})
         city = address.get('city', 'N/A')
         state = address.get('state', 'N/A')
         return city, state
