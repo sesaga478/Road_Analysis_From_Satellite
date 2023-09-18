1cdada8c76910d0f890a55307381b917fdd1eeff
Sebastián Salazar <54647017+sesaga478@users.noreply.github.com>
2023-09-18 10:17:18 -0500

    Update crop_images_gdal.py

--- scripts/crop_images_gdal.py
+++ scripts/crop_images_gdal.py
@@ -2,52 +2,45 @@
 """
 @author: SSALAZAR
 """
-
 from osgeo import gdal
 import numpy as np
 import os,glob, cv2
 from tqdm import tqdm
 
 
-def crop(ORI_PATH="./data/big_img/",SIZE=100,CROP_PATH='crop_images/images', EXT='tif'):
+def crop(img_in="/content/Road_Analysis_From_Satellite/data_example/images/20230221_175753_SN29_L3_SR_MS_11_0.tif",
+         SIZE=100,fol_crop='/content/crop', EXT='tif'):  
 
-    original_path = ORI_PATH
-    fol_crop = os.path.join(CROP_PATH)
-
-    if not os.path.exists(CROP_PATH): # here '100' corresponds to size by default
-        os.makedirs(CROP_PATH)
-
-    list_imgs_original = glob.glob(original_path+'/*.tif')
-
-    for im in tqdm(list_imgs_original):
+    if not os.path.exists(fol_crop): # here '100' corresponds to size by default
+        os.makedirs(fol_crop)
         
-        ds = gdal.Open(im)
+    ds = gdal.Open(img_in)
 
-        num_bands = ds.RasterCount
-        data = []
-        for i in range(num_bands):
-            band = ds.GetRasterBand(i+1)
-            band_data = band.ReadAsArray()
-            data.append(band_data)
-        data = np.array(data)
-        img_np = np.transpose(data, (1, 2, 0))
-        img_width, img_height, dimension = img_np.shape
-        height_sizes = [SIZE]
-        width_sizes =  [SIZE]
-        print(img_np.shape)
-        for height in height_sizes:
-            width=height
-            k = 0
-            for i in range(0, img_height, height):
-                for j in range(0, img_width, width):
-                    try:
-                        imagen_test_name = os.path.join(fol_crop,im.split('/')[-1].replace("."+EXT, '') + '_{}_{}_{}_{}_{}_1.{}'.format(i, j, k, height, width,EXT))
-                        #print(imagen_test_name)
-                        if not os.path.exists(imagen_test_name):
-                            gdal.Translate(imagen_test_name, im,options='-srcwin {} {} {} {}'.format(j, i, width, height))
-                    except:
-                        pass
-                    k+=1
+    num_bands = ds.RasterCount
+    data = []
+    for i in range(num_bands):
+        band = ds.GetRasterBand(i+1)
+        band_data = band.ReadAsArray()
+        data.append(band_data)
+    data = np.array(data)
+    img_np = np.transpose(data, (1, 2, 0))
+    img_width, img_height, dimension = img_np.shape
+    height_sizes = [SIZE]
+    width_sizes =  [SIZE]
+    print(img_np.shape)
+    for height in tqdm(height_sizes):
+        width=height
+        k = 0
+        for i in range(0, img_height, height):
+            for j in range(0, img_width, width):
+                try:
+                    imagen_test_name = os.path.join(fol_crop,img_in.split('/')[-1].replace("."+EXT, '') + '_{}_{}_{}_{}_{}_1.{}'.format(i, j, k, height, width,EXT))
+                    #print(imagen_test_name)
+                    if not os.path.exists(imagen_test_name):
+                        gdal.Translate(imagen_test_name, img_in,options='-srcwin {} {} {} {}'.format(j, i, width, height))
+                except:
+                    pass
+                k+=1
 
 
     image_files = os.listdir(fol_crop)
@@ -63,6 +56,5 @@ def crop(ORI_PATH="./data/big_img/",SIZE=100,CROP_PATH='crop_images/images', EXT
                 
         except:
             pass
-    print(len(image_files))
 
-
+    print(len(os.listdir(fol_crop)))
